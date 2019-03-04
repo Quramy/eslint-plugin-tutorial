f62297bc90488c458230510ad5cb32a7166cb629
Yosuke Kurami <yosuke.kurami@gmail.com>
2019-03-04 15:19:01 +0900

    modify test env

--- src/rules/no-hoge-string.test.ts
+++ src/rules/no-hoge-string.test.ts
@@ -4,19 +4,6 @@ import rule from "./no-hoge-string";
 
 const tester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
 
-// @ts-ignore
-RuleTester.describe = function(text, method) {
-  // @ts-ignore
-  RuleTester.it.title = text;
-  return method.call(this);
-};
-
-// @ts-ignore
-RuleTester.it = function(text, method) {
-  // @ts-ignore
-  test(RuleTester.it.title + ": " + text, method);
-};
-
 tester.run("no-hoge-string", rule, {
   valid: [
     { code: `let x` },
